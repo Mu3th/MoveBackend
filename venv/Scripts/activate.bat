16335d0f8c79a76743d303e07f9ae6534c63a98f
Mu3th <abujalboosh@gmail.com>
2025-07-21 13:22:54 +0300

    aa

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\MAbujalboosh\Desktop\تنقل\BE-Move\venv
+set VIRTUAL_ENV=C:\Users\MAbujalboosh\Desktop\Move\BE-Move\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
